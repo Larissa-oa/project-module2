4a91e36c009249030ef67cf2527be955a8331007
Larissa Almeida <oa.larissa@hotmail.com>
2025-03-28 13:30:24 +0000

    ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../images/gatherlogo1.png";
+import logo from "../images/logo.png";
 
 const Navbar = () => {
   return (
